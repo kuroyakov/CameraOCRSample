63056dd94d23b43908bd45d22ca72d237eb054de
kuroyakov <k.kuroya@gmail.com>
2016-11-24 23:06:23 +0900

    Initial Commit

---
+++ CameraOCR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraOCR
+//
+//  Created by Katsutoshi Kuroya on 2016/11/24.
+//  Copyright © 2016年 Katsutoshi Kuroya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
